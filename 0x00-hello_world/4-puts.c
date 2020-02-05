f6303c25bae6b9c81df2dc6ea504b2c5d8dda077
andresjjn <1453@holbertonschool.com>
2020-02-05 20:38:11 +0000

    4-puts.c 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - iinting with puts function
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
