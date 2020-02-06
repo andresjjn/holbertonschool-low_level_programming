84fbc21702a63f2257e96ca9bf46bc877eb870b8
andresjjn <1453@holbertonschool.com>
2020-02-06 21:03:54 +0000

    2 Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet in lowercase
+ * putchar - Writes a character to the standard output
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar (c);
+	putchar('\n');
+	return (0);
+}
