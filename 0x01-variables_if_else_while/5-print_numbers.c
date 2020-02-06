084453c26632ca3a5092d9a65f055e2fb33b83a6
andresjjn <1453@holbertonschool.com>
2020-02-06 21:28:20 +0000

    5 Print Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet in lowercase
+ * putchar - Writes a character to the standard output
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d", n);
+	printf("\n");
+	return (0);
+}
