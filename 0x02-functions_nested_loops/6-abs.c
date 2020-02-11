897a2dd80b583f97febd23fded11c543e33bfb5b
andresjjn <1453@holbertonschool.com>
2020-02-11 19:46:55 +0000

    6 Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _abs - Absolute value of a number
+ * @n: Number to test
+ * _putchar - writes the character c to stdout
+ * Return: 0
+ */
+int _abs(int n);
+{
+	int n = 0;
+
+	if (n <= 0)
+	n = n * -1;
+	return (n);
+}
