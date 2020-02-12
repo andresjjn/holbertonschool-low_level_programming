d856cdb20fd826e7575a194a3d7efe33056dba73
Your Name <you@example.com>
2020-02-12 11:35:42 -0500

    2 largest 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 
 /**
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -22,6 +23,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
-	return(largest);
+
+	return (largest);
 }
