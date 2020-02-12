198560e207a68ab7d2be14daccb777c610d71c5a
Your Name <you@example.com>
2020-02-11 23:40:46 -0500

    9 Times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * times_table - Prnt table from 0 to 9
+ * print_putchar - Print numbers just with _putchar
+ * _putchar - writes the character c to stdout
+ * Return: 0
+ */
+void times_table(void)
+{
+	long p, i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+		p = (i * j);
+		print_putchar(p);
+		if (j != 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		}
+	_putchar('\n');
+	}
+}
