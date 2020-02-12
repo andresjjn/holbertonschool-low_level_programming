9ad26533389278c8e2a332abeb9d8dee5f37cbab
Your Name <you@example.com>
2020-02-12 10:43:26 -0500

    1 main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - Infinite loop
+ * return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	printf("InInfinite loop incoming :(\n");
+	i = 0:
+	printf("Infinite loop avoided! \\o\/\n");
+	return (0);
+}
