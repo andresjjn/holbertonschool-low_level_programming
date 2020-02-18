f3fdb947b655ae6f92c8043b3aa3559ba508acbb
andresjjn <1453@holbertonschool.com>
2020-02-18 18:26:11 +0000

    Add 1 swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: First variable to swap.
+ * @b: Second variable to swap.
+ * _putchar - Write characters
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
