21c2d8fe5b968b73e1e1c8cf96c28fbde705257c
andresjjn <1453@holbertonschool.com>
2020-02-18 16:04:09 +0000

    Add README.md and 0-Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Change the value of a variable through a pointer.
+ * @*n: Pointer
+ * _putchar - writes the character c to stdout
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
