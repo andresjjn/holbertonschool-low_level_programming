8cf8e3cd4910c7b2b69bfaf4b6da4b59ca461aa3
andresjjn <1453@holbertonschool.com>
2020-02-18 22:40:15 -0500

    Add 7-Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,14 +11,12 @@ void puts_half(char *str)
 	int i = 0, j = 0, n;
 
 	while (str[i] != '\0')
-	{
 		i++;
-	}
 	if (i % 2 == 0)
 		n = i / 2;
 	else
-		n = (i - 1) / 2;
-	for (j = n; j  <=  i - 1; j++)
+		n = (i + 1) / 2;
+	for (j = n; j  <  i; j++)
 	{
 		_putchar(str[j]);
 	}
