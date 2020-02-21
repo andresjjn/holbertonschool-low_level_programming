7bb0f769a740935c575551428afa84a004f7acd6
andresjjn <1453@holbertonschool.com>
2020-02-21 19:13:47 +0000

    Add 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	const unsigned char *p1 = (const unsigned char *)s1;
-	const unsigned char *p2 = (const unsigned char *)s2;
 
-	while (*p1 != '\0')
+	for (; s1[i] != '\0', i++)
 	{
-		if (*p2 == '\0')
-			return (15);
-		if (*p2 > *p1)
-			return (-15);
-		if (*p1 > *p2)
-			return (15);
-		p1++;
-		p2++;
+		if (s1[i] != s2 [i])
+			return (s1[i] - s2[i]);
 	}
-	if (*p2 != '\0')
-		return (-15);
 	return (0);
 }
