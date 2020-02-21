f1eb2072b3bad7ed43433f3ee6401293566c1d75
andresjjn <1453@holbertonschool.com>
2020-02-21 20:22:56 +0000

    Add 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,26 +4,23 @@
  * _strcmp - Compare two strings.
  * @s1: String to campare.
  * @s2: String to compare.
- * Return: dest 
+ * Return: dest
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	*p1 = *s1;
-	*p2 = *s2;
 
-	while (*p1 != '\0')
+	while (s1[i] != '\0')
 	{
-		if (*p2 == '\0')
+		if (s2[i] == '\0')
 			return (15);
-		if (*p2 > *p1)
+		if (s2[i] > s1[i])
 			return (-15);
-		if (*p1 > *p2)
+		if (s1[i] > s2[i])
 			return (15);
-		p1++;
-		p2++;
+		i++;
 	}
-	if (*p2 != '\0')
-		return -1;
-	return 0;
+	if (s2[i] != '\0')
+		return (-15);
+	return (0);
 }
