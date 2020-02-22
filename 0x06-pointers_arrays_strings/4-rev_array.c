40c9af3cdf3eba900cf945367af99be82bb997ae
andresjjn <1453@holbertonschool.com>
2020-02-21 22:31:47 -0500

    Add 4- RevArray

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Reverse an array.
+ * @a: Array to reverse.
+ * @n: The size of a.
+ * Return: void.
+ */
+void reverse_array(int *a, int n)
+{
+	int k = 0, au;
+	
+	while (k < n--)
+	{
+		au = a[k];
+		a[k++] = a[n];
+		a[n] = au;
+	}	
+}
