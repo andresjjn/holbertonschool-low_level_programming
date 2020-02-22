ad75c7ce00d643952152c924b0c66e610df87a50
andresjjn <1453@holbertonschool.com>
2020-02-21 22:06:45 -0500

    Add 3-Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1l = 0, s2l 0;
-	char *s1s = s1, *s2s =s2;
+	int s1l = 0, s2l = 0;
+	char *s1s = s1, *s2s = s2;
 
 	while (*s1)
 	{
@@ -19,20 +19,20 @@ int _strcmp(char *s1, char *s2)
 	s1 = s1s;
 	while (*s2)
 	{
-	s2l = s2l + 1;
-	s2++;
+		s2l = s2l + 1;
+		s2++;
 	}
 	s2 = s2s;
 	if (s1 > s2)
-		return (15);
-	else if (s1 < s2)
 		return (-15);
+	else if (s1 < s2)
+		return (15);
 	while (*s1)
 	{
 		if (*s1 < *s2)
-			return (-15);
-		if (*s1 > *s2)
 			return (15);
+		if (*s1 > *s2)
+			return (-15);
 		s1++;
 		s2++;
 	}
