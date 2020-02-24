ea5e137a2bfed7942ba44045e62f42168d7c84cb
andresjjn <1453@holbertonschool.com>
2020-02-24 18:28:04 +0000

    Add 2-strchrwq

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Function that locates a character in a string..
+ * @s: Input string.
+ * @c: Character to locates.
+ * Return: dest.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			break;
+		i++;
+	}
+	return (&s[i]);
+}
