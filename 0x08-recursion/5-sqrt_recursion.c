bb000bcfee837ad28f3927d73691a681cd2c3211
andresjjn <1453@holbertonschool.com>
2020-02-26 22:47:18 -0500

    Add 5-sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * srt - Return
+ * @n : Number
+ * @i : Aux number
+ * Return: Res
+ */
+int srt(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n < (i * i))
+		return (-1);
+	else
+ 		return (srt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - Function that returns the natural square root of a number.
+ * @n: Number to apply square root.
+ * Return: Square root.
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+	int t;
+
+	t = srt(n, i);
+	return (t);
+}
