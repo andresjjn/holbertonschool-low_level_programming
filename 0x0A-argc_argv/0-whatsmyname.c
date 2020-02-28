4a2fc4e1748577800e4c39514af7450efcc7c9a3
andresjjn <1453@holbertonschool.com>
2020-02-28 21:04:32 +0000

    Add README.md, 0-whatsmyname.c and holberton.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name, followed by a new line.
+ * @argc: Number of command line arguments.
+ * @argv: Array containing the program command line arguments.
+ * Return: 0.
+ */
+int main (int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
