615d0bab7076f84f9d21876ec7d5580f81e3e044
andresjjn <1453@holbertonschool.com>
2020-03-01 22:31:48 +0000

    Add 2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,12 +10,9 @@ int main(int argc, char *argv[])
 {
 	int count;
 
-	if (argc > 1)
+	for (count = 0; count < argc; count++)
 	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("%s\n", argv[count]);
-		}
+		printf("%s\n", argv[count]);
 	}
 	return (0);
 }
