5ab7de645c3b306ed97e95a76138e1296a7c6d1b
andresjjn <1453@holbertonschool.com>
2020-03-05 01:20:46 +0000

    Add 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: 2 dimensional array.
+ * @height: Number of matrix rows.
+ * Return: void.
+ */
+void free_grid(int **grid, int height)
+{
+	int j;
+
+	for (j = 0; j < height; j++)
+		free(grid[j]);
+	free(grid);
+}
