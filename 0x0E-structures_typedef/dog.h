aee5198109b9a56f840abcb03889f5238a315d0a
andresjjn <1453@holbertonschool.com>
2020-03-09 20:14:25 +0000

    0-main.c, dog.h and README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner
+ *
+ * Description: Dog object
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H */
