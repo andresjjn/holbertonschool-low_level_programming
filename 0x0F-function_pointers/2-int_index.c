8d060c608c2d21b34bfa532d77707c4f69158939
andresjjn <1453@holbertonschool.com>
2020-03-11 19:46:16 +0000

    Add 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches for an integer.
+ * @array: Pointer of an array.
+ * @size: Size of string
+ * @cmp: Function pointer
+ * Return: -
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if ((array == NULL) || (cmp == NULL))
+		return (0);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
