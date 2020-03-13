6bb9d89e4d75e2d2c6948015585e63e6942a99ff
andresjjn <1453@holbertonschool.com>
2020-03-13 18:09:34 +0000

    Add 0-sum_them_all.c, variadic_functions.h and README.md

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,6 +10,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_list arg;
 	unsigned int i, sum = 0;
 
+	if (n == 0)
+		return (0);
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 		sum = sum + va_arg(arg, int);
