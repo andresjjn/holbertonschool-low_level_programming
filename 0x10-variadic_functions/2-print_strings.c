2992dae3b5c2c96f24b45b6b0ac1fa3df1b44057
andresjjn <1453@holbertonschool.com>
2020-03-14 22:06:47 +0000

    Fixed: 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Function that prints numbers, followed by a new line.
+ * print_strings - Function that strings or (nill) if string = NULL.
  * @separator: String to be printed between numbers.
  * @n: Number of integers passed to the function.
  * Return: -
@@ -12,16 +12,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *s;
 
-	if (n == 0)
-		return;
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(arg, char*);
-		printf("%s", s);
+		if (s != NULL)
+			printf("%s", s);
+		else
+			printf("(nil)");
 		if ((separator != NULL) && (i != n - 1))
 			printf("%s", separator);
 	}
-	printf("\n");
 	va_end(arg);
+	printf("\n");
 }
