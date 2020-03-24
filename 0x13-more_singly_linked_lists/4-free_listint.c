72de3b64f55777752a97aeffc5182235fc9f49d9
andresjjn <1453@holbertonschool.com>
2020-03-24 00:16:48 +0000

    Add 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_list - Free the memory space in a linked list
+ *@head: head of the linked list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
