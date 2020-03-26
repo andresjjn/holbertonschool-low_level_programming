c5ea42abfd1444a499e319965aa96ebad438a105
andresjjn <1453@holbertonschool.com>
2020-03-26 20:23:36 +0000

    Add 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,25 +3,13 @@
 /**
  * iterate - Iterate to print.
  * @n: Number to convert.
- * Return: Iteration or 0.
  */
-unsigned long int iterate(unsigned long int n)
+void iterate(unsigned long int n)
 {
 	if (n == 0)
-		return (0);
-	else
-		return (n % 2 + 10 * iterate(n / 2));
-}
-
-/**
- * print_number - Function that print integers
- * @n: Integer to print
- */
-void print_number(unsigned long int n)
-{
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+		return;
+	iterate(n >> 1);
+	_putchar((n & 1) + '0');
 }
 
 /**
@@ -30,8 +18,8 @@ void print_number(unsigned long int n)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int r;
-
-	r = iterate(n);
-	print_number(r);
+	if (n == 0)
+		_putchar('0');
+	else
+		iterate(n);
 }
