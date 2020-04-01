9b3c7ad2b0ec4916ab598493be805dadb20e514a
andresjjn <1453@holbertonschool.com>
2020-04-01 03:36:37 +0000

    Fix: Malloc at 0-read_textfile.c4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it.
  * @filename: Name of file to read and print.
  * @letters: Number of letters it should read and print.
  * Return: Number of letters it could read and print or 0 if fail.
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY, 600);
 	if (fd == -1)
 		return (0);
-        buffer = (char *)malloc(letters * sizeof(char));
+	buffer = (char *)malloc(letters * sizeof(char));
 	if (buffer == NULL)
 		return (0);
 	_read = read(fd, buffer, letters);
