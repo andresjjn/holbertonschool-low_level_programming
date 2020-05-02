1026f07b1bd7fbb1e562110d0baba7bdc172a6e6
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-02 21:58:20 +0000

    Add: 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - Function that free a dlistint_t list.
+ * @head: Poiter to first node of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+        dlistint_t *temp;
+
+        while (head != NULL)
+        {
+                temp = head->next;
+                free(head);
+                head = temp;
+        }
+	free(head);
+}
