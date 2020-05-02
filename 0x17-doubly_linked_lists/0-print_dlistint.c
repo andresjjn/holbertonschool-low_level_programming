cf5afb9271afc0b1da038f6ec04f10b4bb1de5d4
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-02 17:45:35 +0000

    Add README.md, lists.h, and 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,32 @@
 #include "lists.h"
 /**
+ * printint - Function that prints integers.
+ * @n: Integer to print.
+ */
+void printint(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+		printint(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+/**
  * print_dlistint - Function that prints all the elements of a dlistint_t list.
  * @h: Header pointer of list.
  * Return: The number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int j = 0;
+	size_t j = 0;
 
 	while (h != NULL)
 	{
-		_putchar('0' + h->n);
+		printint(h->n);
 		_putchar('\n');
 		h = h->next;
 		j++;
