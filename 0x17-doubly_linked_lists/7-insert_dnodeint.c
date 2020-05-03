c9f45b0a32435f02528248c6bbe6d82ff8268f10
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-03 00:00:17 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *tmp;
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL || h == NULL)
 		return (NULL);
 	new->n = n;
 	if (idx == 0)
