ceaa9b4118bcb235d314a73707512caada26656d
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-03 15:28:42 +0000

    Fix: 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,45 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Function that inserts a new node in a index.
+ * insert_dnodeint_at_index - Function that inserts a new_node node in a index.
  * @h: Pointer of a linked list.
- * @idx: Index to add new node
+ * @idx: Index to add new_node node
  * @n: Integer to save
- * Return: Pointer of new node added
+ * Return: Pointer of new_node node added
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp;
+	dlistint_t *new_node, *tmp = *h;
+	unsigned int i = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL || h == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
-	if (idx == 0)
+	new_node->n = n;
+	if (idx == 0 || *h == NULL)
 	{
-		if (*h != NULL)
-		{
-			h[0]->prev = new, new->next = *h;
-		}
-		else
-			new->next = NULL;
-		new->prev = NULL;
-		*h = new;
-		return (new);
+		free(new_node);
+		return (add_dnodeint(h, n));
 	}
-	if (*h == NULL)
-		return (NULL);
 	tmp = *h;
-	while (tmp->prev)
-		tmp = tmp->prev;
-	while (tmp != NULL)
+	while (tmp && i < idx - 1)
 	{
-		if (idx == 0)
+		if (tmp->next == NULL)
 		{
-			tmp->prev->next = new, new->prev = tmp->prev, tmp->prev = new;
-			new->next = tmp;
-			return (new);
+			free(new_node);
+			return (NULL);
 		}
-		new->prev = tmp;
-		tmp = tmp->next, idx--;
+		else if (tmp->next->next == NULL)
+		{
+			free(new_node);
+			return (add_dnodeint_end(h, n));
+		}
+		tmp = tmp->next;
+		i++;
 	}
-	if (!idx)
-	{
-		new->prev->next = new, new->next = NULL;
-		return (new);
-	}
-	return (NULL);
+	new_node->prev = tmp;
+	new_node->next = tmp->next;
+	tmp->next->prev = new_node;
+	tmp->next = new_node;
+	return (new_node);
 }
