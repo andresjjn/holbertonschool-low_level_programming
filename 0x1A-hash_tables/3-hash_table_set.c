62a7a9e06beed562ae4c4e9e3b2fb15cf2c9cdce
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-29 22:22:03 +0000

    Add: 4-hash_table_get.c Fix: 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *tmp, *new_node;
 	unsigned long int k;
 
-	if (!ht || !value || strlen(key) == 0)
+	if (!ht || !value || key[0] == '\0')
 		return (0);
 	k = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[k];
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Function that retrieves a value associated with a key
+ * @ht: The hash table you want to look into
+ * @key: The key you are looking for
+ * Return: Value associated or NULL if key couldn’t be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int k = 0;
+	const hash_node_t *tmp;
+
+	if (!ht || !key)
+		return (0);
+	k = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[k];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
