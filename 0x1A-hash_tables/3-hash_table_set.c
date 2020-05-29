335dede56258b66b1088c16b1784bf7d2b0702d4
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-29 21:45:59 +0000

    Fix: 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,10 +30,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node = malloc(sizeof(hash_node_t));
 		if (new_node == NULL)
-			return (NULL);
+			return (0);
 		new_node->key = strdup(key);
 		new_node->value = strdup(value);
-		new->next = ht->array[k];
+		new_node->next = ht->array[k];
 		ht->array[k] = new_node;
 		return (1);
 	}
