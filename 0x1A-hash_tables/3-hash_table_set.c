6ba630ea4fdc46cfca950df8e8cda8e922aa1a83
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-29 21:44:33 +0000

    Fix: 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,34 +10,31 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int k = 0;
+	hash_node_t *new_node;
 
 	if (key[0] == '\0' || ht == NULL || value == NULL || key == NULL)
 		return (0);
 	k = key_index((unsigned char *)key, ht->size);
-	ht->array[k] = add_node(&ht->array[k], (char *)key, (char *)value);
 	if (ht->array[k] == NULL)
-		return (0);
-	return (1);
-}
-
-
-/**
- *add_node - adds a new node to the begging of a given linked list
- *@array: head of the linked list
- *@key: Key of hash table.
- *@value: string to be placed in the memory space
- *Return: a pointer to the new head
- */
-hash_node_t *add_node(hash_node_t **array, char *key, char *value)
-{
-	hash_node_t *new_node;
-
-	new_node = calloc(1, sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = *array;
-	*array = new_node;
-	return (*array);
+	{
+		new_node = calloc(1, sizeof(hash_node_t));
+		if (new_node == NULL)
+			return (0);
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
+		new_node->next = NULL;
+		ht->array[k] = new_node;
+		return (1);
+	}
+	else
+	{
+		new_node = malloc(sizeof(hash_node_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
+		new->next = ht->array[k];
+		ht->array[k] = new_node;
+		return (1);
+	}
 }
