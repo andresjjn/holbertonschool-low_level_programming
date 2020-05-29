6c9a040aaa631bfbdca6e70def37fc7e2f999a95
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-29 15:13:13 +0000

    Add: 0-hash_table_create.c, README.md and hash_tables.h

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Function that creates a hash table.
+ * @size: Size of array.
+ * Return:  Pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable = NULL;
+	unsigned long int i;
+
+	hashtable = malloc(sizeof(hash_table_t));
+	hashtable->array = malloc(sizeof(hash_node_t) * size);
+	for (i = 0; i < size; i++)
+		hashtable->array[i] = NULL;
+	return (hashtable);
+}
