c57d76e5a5ab4de79a61c16d32deb91f66b27199
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-05-29 19:08:40 +0000

    Fix: 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int k = 0;
 
-	if (key == NULL || key[0] == '\0')
+	if (key == NULL || key[0] == '\0' || ht == NULL)
 		return (0);
 	k = key_index((unsigned char *)key, ht->size);
 	ht->array[k] = add_node(&ht->array[k], (char *)key, (char *)value);
