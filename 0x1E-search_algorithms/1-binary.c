2cae3b1dc34db1114911e455c147f8fdbc579a57
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-08-26 19:01:21 +0000

    Fix: 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 
 	a = 0;
 	b = size - 1;
-	if (array == NULL || size == 0)
+	if (array == NULL || size <= 0)
 		return (-1);
 	while (a <= b)
 	{
